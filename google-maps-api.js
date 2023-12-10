2721fbfb5d467dc9f45e5dbcc90f5c6162386ffd
davonbl-bl <davonbl@gmail.com>
2023-12-10 13:35:46 -0500

    addeding coordinates which then translates to a marker appended on the google maps interface

--- google-maps-api.js
+++ google-maps-api.js
@@ -1,12 +1,27 @@
-let map;
+// let map;
 
-async function initMap() {
+async function initMap(testing) {
   const { Map } = await google.maps.importLibrary("maps");
-  const eastVilliageBooks = { lat: 40.727361, lng: -73.984396  };
-  map = new Map(document.getElementById("map"), {
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker")
+
+  const eastVilliageBooks = { lat: 40.727405, lng: -73.984462 };
+
+  const map = new Map(document.getElementById("map"), {
     center: eastVilliageBooks,
     zoom: 13,
+    // mapId: "DEMO_MAP_ID"
   });
+
+  new google.maps.Marker({
+    position: eastVilliageBooks,
+    map: map,
+  })
+
+//   const marker = new AdvancedMarkerElement({
+//     map: map,
+//     position: eastVilliageBooks
+//     // title: "Uluru",
+//   })
 }
 
 initMap();
